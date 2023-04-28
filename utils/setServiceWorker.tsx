0fde8fd231ec8e982a4a935ad6aec71f4ac1531f
naruhiko <39730309+naruhiko-toda@users.noreply.github.com>
2023-04-28 23:18:41 +0900

    Update setServiceWorker.tsx

--- utils/setServiceWorker.tsx
+++ utils/setServiceWorker.tsx
@@ -1,16 +1,1 @@
-export function setServiceWorker() {
-  console.log("start setServiceWorker")
-  console.log(navigator)
-  console.log('serviceWorker' in navigator)
-  if ('serviceWorker' in navigator) {
-    console.log("this application has service worker, so please install!!")
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
-  }
-}
+

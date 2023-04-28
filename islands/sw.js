1bbe7971cd99c4d0ed2eb0bc35c3afc7e446e4f3
naruhiko <39730309+naruhiko-toda@users.noreply.github.com>
2023-04-28 23:22:41 +0900

    Create sw.js

---
+++ islands/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.tsx',
+        '/logo.svg',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});

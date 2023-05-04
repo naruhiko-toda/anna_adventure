f86a18ced9c01602f92be75784fc2be6c37af32a
naruhiko-toda <nr27bypphy@gmail.com>
2023-05-04 23:19:11 +0900

    keyを推したらその方向に動くようにする

--- islands/Main.tsx
+++ islands/Main.tsx
@@ -3,8 +3,22 @@ import { useEffect, useRef } from "preact/hooks";
 export default function Main() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const imageRef = useRef<HTMLImageElement | null>(null);
+  let input_key_buffer = {};
 
   useEffect(() => {
+    // キーボードの入力イベントをトリガーに配列のフラグ値を更新させる
+    window.addEventListener("keydown", handleKeydown);
+    function handleKeydown(e) {
+      console.log("key down : " + e.keyCode);
+      input_key_buffer[e.keyCode] = true;
+    }
+
+    window.addEventListener("keyup", handleKeyup);
+    function handleKeyup(e) {
+      console.log("key up : " + e.keyCode);
+      input_key_buffer[e.keyCode] = false;
+    }
+
     if (canvasRef.current) {
       const ctx = canvasRef.current.getContext("2d");
 
@@ -16,15 +30,33 @@ export default function Main() {
         };
       }
       let x = 0;
+      let y = 0;
 
       function refresh() {
         console.log("refresh")
         ctx?.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-        x = x + 1;
+        if (input_key_buffer[37]) {
+          // 左が押されていればx座標を1減らす
+          x--;
+        }
+        if (input_key_buffer[38]) {
+          // 上が押されていればy座標を1減らす
+          y--;
+        }
+        if (input_key_buffer[39]) {
+          // 右が押されていればx座標を1増やす
+          x++;
+        }
+        if (input_key_buffer[40]) {
+          // 下が押されていればy座標を1増やす
+          y++;
+        }
 
         const image = imageRef.current;
+        console.log("x=", x)
+        console.log("y=", y)
         if (image) {
-          ctx?.drawImage(image, x, 0, image.width * 0.1, image.height * 0.1);
+          ctx?.drawImage(image, x, y, image.width * 0.1, image.height * 0.1);
         }
         window.requestAnimationFrame(refresh);
       }
